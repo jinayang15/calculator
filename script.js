001610784aaeb37c2398510602aadcef564c5768
Jina Yang <jinayang15@gmail.com>
2024-05-19 15:03:01 -0400

    Add operate function to call basic operations

--- script.js
+++ script.js
@@ -8,5 +8,28 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
+    if (b == 0) return NaN;
     return a / b;
 }
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return subtract(a,b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+    }   
+}
+
+function main() {
+}
+
+main()
