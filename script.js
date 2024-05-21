98552185c35d87cc9ac4a82d8c1327e89e28a377
Jina Yang <jinayang15@gmail.com>
2024-05-21 16:52:20 -0400

    Implement basic operations with integers

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b, operator) {
+function operate(a, operator="=", b = a) {
+    a = +a;
+    b = +b;
     switch (operator) {
         case "+":
             return add(a,b);
@@ -20,16 +22,69 @@ function operate(a, b, operator) {
         case "-":
             return subtract(a,b);
             break;
-        case "*":
+        case "×":
             return multiply(a,b);
             break;
-        case "/":
+        case "÷":
             return divide(a,b);
             break;
+        case "=":
+            return a;
     }   
 }
 
+function addButtonEventListener(buttons) {
+    
+}
+
 function main() {
+    const display = document.querySelector(".display");
+    const buttons = document.querySelectorAll(".button");
+    let operatorSelected = false;
+    let expression = "0";
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const buttonClassList = button.classList;
+            if (buttonClassList.contains("num")) {
+                if (expression == "0" && button.id != "decimal") {
+                    expression = button.textContent;
+                    display.textContent = button.textContent;
+                }
+                else if (operatorSelected) {
+                    expression += button.textContent;
+                    display.textContent = button.textContent;
+                    operatorSelected = false;
+                }
+                else {
+                    expression += button.textContent;
+                    display.textContent += button.textContent;
+                }
+            }
+            else if (buttonClassList.contains("operator")) {
+                switch (button.id) {
+                    case "equal":
+                        const expressionArr = expression.match(/(\d+|\+|-|×|÷)/g);
+                        alert(expressionArr);
+                        const result = operate(...expressionArr);
+                        alert(result);
+                        expression = result;
+                        display.textContent = result;
+                        break;
+                    case "clear":
+                        expression = "0";
+                        display.textContent = "0";
+                        break;
+                    case "divide":
+                    case "multiply":    
+                    case "plus":
+                    case "minus":
+                        operatorSelected = true;
+                        expression += button.textContent;
+                        break;
+                }
+            }
+        });
+    });
 }
 
 main()
