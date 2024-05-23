ff2134ddd79c0c71a5c0194f013bddd59f7a2258
Jina Yang <jinayang15@gmail.com>
2024-05-23 09:52:56 -0400

    Organize code and add a couple handy features
    - Allows for expressions to be continuously evaluated without pressing
    the equal sign. ex. 5+6-7*8= becomes 11-7*8 -> 4*8 -> 32
    - If an operator is clicked but another operator was previously selected,
    the last operator would be replaced by the most recently pressed one
    - Rounds very long decimal numbers

--- script.js
+++ script.js
@@ -12,29 +12,97 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, operator="=", b = a) {
+function operate(a, operator = "=", b = a) {
     a = +a;
     b = +b;
     switch (operator) {
         case "+":
-            return add(a,b);
+            return add(a, b);
             break;
         case "-":
-            return subtract(a,b);
+            return subtract(a, b);
             break;
         case "×":
-            return multiply(a,b);
+            return multiply(a, b);
             break;
         case "÷":
-            return divide(a,b);
+            return divide(a, b);
             break;
         case "=":
             return a;
-    }   
+    }
 }
 
-function addButtonEventListener(buttons) {
-    
+function calculateResult(expressionArr) {
+    // number of digits that can be displayed on the calculator accounting for the decimal point
+    const digitLimit = 11; 
+    // round number to prevent overflow
+    const result = Math.round(operate(...expressionArr) * Math.pow(10, digitLimit - 1)) / Math.pow(10, digitLimit - 1);
+    return result;
+}
+
+function onNumClick(button, operatorSelected, expression, display) {
+    // replaces the number on the display with the next number click if the current expression is 0
+    if (expression == "0" && button.id != "decimal") {
+        expression = button.textContent;
+        display.textContent = button.textContent;
+    }
+    // displays the new number after operator has been clicked
+    else if (operatorSelected) {
+        expression += button.textContent;
+        display.textContent = button.textContent;
+        operatorSelected = false;
+    }
+    else {
+        expression += button.textContent;
+        display.textContent += button.textContent;
+    }
+    return {
+        expression: expression,
+        operatorSelected: operatorSelected
+    }
+}
+
+function onOperatorClick(button, operatorSelected, expression, display) {
+    // regex to split string into numbers and operators
+    const expressionArr = String(expression).match(/(\d+(\.\d+)?)|[\+-×÷]/g);
+    switch (button.id) {
+        case "equal":
+            const result = calculateResult(expressionArr);
+            expression = result;
+            display.textContent = result;
+            operatorSelected = false;
+            break;
+        case "clear":
+            expression = "0";
+            display.textContent = "0";
+            operatorSelected = false;
+            break;
+        case "divide":
+        case "multiply":
+        case "plus":
+        case "minus":
+            // replaces previous operator when another is clicked
+            if (operatorSelected) {
+                expression = expression.slice(0,-1) + button.textContent;   
+            }
+            // evaluate the result before continuing other operations even if equal sign is not clicked
+            else if (expressionArr.length == 3) {
+                const result = calculateResult(expressionArr);
+                expression = result + button.textContent;
+                display.textContent = result;
+                operatorSelected = true;
+            }
+            else {
+                expression += button.textContent;
+                operatorSelected = true;
+            }
+            break;
+    }
+    return {
+        expression: expression,
+        operatorSelected: operatorSelected
+    }
 }
 
 function main() {
@@ -42,47 +110,18 @@ function main() {
     const buttons = document.querySelectorAll(".button");
     let operatorSelected = false;
     let expression = "0";
+
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
-            const buttonClassList = button.classList;
-            if (buttonClassList.contains("num")) {
-                if (expression == "0" && button.id != "decimal") {
-                    expression = button.textContent;
-                    display.textContent = button.textContent;
-                }
-                else if (operatorSelected) {
-                    expression += button.textContent;
-                    display.textContent = button.textContent;
-                    operatorSelected = false;
-                }
-                else {
-                    expression += button.textContent;
-                    display.textContent += button.textContent;
-                }
+            let returned;
+            if (button.classList.contains("num")) {
+                returned = onNumClick(button, operatorSelected, expression, display);
             }
-            else if (buttonClassList.contains("operator")) {
-                switch (button.id) {
-                    case "equal":
-                        const expressionArr = expression.match(/(\d+|\+|-|×|÷)/g);
-                        alert(expressionArr);
-                        const result = operate(...expressionArr);
-                        alert(result);
-                        expression = result;
-                        display.textContent = result;
-                        break;
-                    case "clear":
-                        expression = "0";
-                        display.textContent = "0";
-                        break;
-                    case "divide":
-                    case "multiply":    
-                    case "plus":
-                    case "minus":
-                        operatorSelected = true;
-                        expression += button.textContent;
-                        break;
-                }
+            else if (button.classList.contains("operator")) {
+                returned = onOperatorClick(button, operatorSelected, expression, display);
             }
+            expression = returned.expression;
+            operatorSelected = returned.operatorSelected;
         });
     });
 }
